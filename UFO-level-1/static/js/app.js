3fe399aae788a5728f915a048f6898f48582b78e
Felipe Lopez <mr.felipelopez@gmail.com>
2020-12-03 22:47:01 -0600

    finished hw

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,37 +1,44 @@
-const tableData = data
-const tbody = d3.select('tbody')
 
-function buildTable(data) {
-    tbody.html('')  // Clear existing data
-
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
-        })
-    })
-}
-
-const handleClick = () => {
-    d3.event.preventDefault()
-
-    const date = d3.select('#datetime').property('value')
-    let filteredData = tableData;
-
-    if (date)
-        filteredData = filteredData.filter(row => row.datetime === date)
+function tableData(data) {
     
-    tbody.html('')  // Clear existing data
+    let table = d3.select("#ufo-table")
 
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
-        })
+    table.selectAll("tr").remove()
+    
+    // set up header row
+    let headerRow = table.append("tr")
+
+    headerRow.append("th").text("Datetime")
+    headerRow.append("th").text("City")
+    headerRow.append("th").text("State")
+    headerRow.append("th").text("Country")
+    headerRow.append("th").text("Shape")
+    headerRow.append("th").text("Duration (min)")
+    headerRow.append("th").text("Comments")
+
+    // set up table body
+    let tableBody = table.append("tbody")
+
+    data.forEach((d) => {
+        let row = tableBody.append("tr")
+        row.append("td").text(d.datetime)
+        row.append("td").text(d.city)
+        row.append("td").text(d.state)
+        row.append("td").text(d.country)
+        row.append("td").text(d.shape)
+        row.append("td").text(d.durationMinutes)
+        row.append("td").text(d.comments)
     })
+
 }
 
-d3.selectAll('#filter-btn').on('click', handleClick)
-// buildTable(tableData)
+// create function to filter by date input
+function handleClick() {
+    let dateInput = d3.select("#datetime").property("value")
+    let filteredDate = data.filter((d) => d.datetime === dateInput)
+
+    tableData(filteredDate)
+}
+
+// pass id for button and function to click
+d3.select("#filter-btn").on("click", handleClick)
